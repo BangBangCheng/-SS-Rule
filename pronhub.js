67a51d335317c889dc7a89e39f8a38ca2be95611
BangBangCheng <616092088@qq.com>
2022-12-30 14:38:10 +0800

    Update pronhub.js

--- pronhub.js
+++ pronhub.js
@@ -1,3 +1,3 @@
 hostname=*pornhub.com*
 ^https:\/\/(cn|www)\.pornhub\.com\/_xa\/ads url reject-dict
-^https?:/\/(cn|www)\.pornhub\.com\/($|(view|video).*$) url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/pornhub.js
+^https?:/\/(cn|www)\.pornhub\.com\/($|(view|video).*$) url script-response-body https://raw.githubusercontent.com/BangBangCheng/-SS-Rule/master/pronhub.css
