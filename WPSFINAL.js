32455f50cb4634aa375d8f2f9b51eecf8ea84487
BangBangCheng <616092088@qq.com>
2023-01-02 10:56:09 +0800

    Create WPSFINAL.js

---
+++ WPSFINAL.js
@@ -0,0 +1,10 @@
+#!name=WPS会员
+#!desc=WPS解锁本地会员
+#!icon= https://raw.githubusercontent.com/deezertidal/private/main/icons/wps.png
+#!homepage=https://raw.githubusercontent.com/BangBangCheng/-SS-Rule/master/wps11.25.0.js
+
+
+^https?:\/\/.*?account\.wps\.(com|cn)(:\d+)?\/api\/users\/\w+\/overview$ url script-response-body https://raw.githubusercontent.com/I-am-R-E/Functional-Store-Hub/Master/WPSOffice/Script/WPS.js
+
+
+hostname = account.wps.cn *account.wps.com
